7882d1c152bab12b44013c53d62e542b965ce102
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-10 23:32:14 +0530

    Add files via upload

---
+++ function pt2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void printstuff(){    // there are no parameters............
+	cout<<"Writing my first functoin";
+}
+	int main(){
+		printstuff();
+		return 0;
+	}
+
